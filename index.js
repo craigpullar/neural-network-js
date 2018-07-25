0214ca5f2544277e4e2e8d25351456129776386d
Craig Pullar <craigpullar@Craigs-MacBook-Pro.local>
2018-07-25 08:52:24 -0400

    neural network

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const rand = ({ high, low }) => Math.random() * (high - low) + low;
 const createRandomPoint = ({ Y_MAX, X_MAX, Y_LOW = 0, X_LOW = 0 }) => ({
   x: rand({ low: X_LOW, high: X_MAX }),
   y: rand({ low: Y_LOW, high: Y_MAX })
-})
-const weights = createRandomPoint({ Y_MAX: 1, X_MAX: 1, X_LOW: -1, Y_LOW: -1 });
+});
 
 
-const randomPoints = Ramda.range(0, 100).map(() => createRandomPoint({ Y_MAX: 400, X_MAX: 400 }));
+const generatePoints = ({ count }) => Ramda.range(0, count).map(() => createRandomPoint({ Y_MAX: 400, X_MAX: 400 }));
 
 const getTeamForPoint = ({ x, y }) => x > y ? 1 : -1;
+
 const guessTeamForPoint = ({ weights, point: {x, y} }) => {
   const sum = x * weights.x + y * weights.y;
   const team = sum >= 0 ? 1 : -1;
@@ -25,24 +25,35 @@ function train({ weights, point, actualTeam }) {
   const guessResult = guessTeamForPoint({ weights, point });
   const error = actualTeam - guessResult;
   return {
-    x: weights.x + (point.x * error),
-    y: weights.y + (point.y * error)
+    x: weights.x + point.x * error,
+    y: weights.y + point.y * error
   };
 }
 
-const testTrain = () => {
-  const point = { x: 200, y: 400};
-  return train({ weights, point, actualTeam: getTeamForPoint(point)});
-}
-
-const trainedWeights = () => {
-  let trainedWeights;
-  trainedWeights = train({ weights, point: randomPoints[0], actualTeam: getTeamForPoint(randomPoints[0])});
-  trainedWeights = train({ weights, point: randomPoints[1], actualTeam: getTeamForPoint(randomPoints[1])});
-  trainedWeights = train({ weights, point: randomPoints[2], actualTeam: getTeamForPoint(randomPoints[2])});
-  trainedWeights = train({ weights, point: randomPoints[3], actualTeam: getTeamForPoint(randomPoints[3])});
-  trainedWeights = train({ weights, point: randomPoints[4], actualTeam: getTeamForPoint(randomPoints[4])});
+const trainWeights = () => {
+  const randomPoints = generatePoints({ count: 1000 })
+  const startingWeights = createRandomPoint({ Y_MAX: 1, X_MAX: 1, X_LOW: -1, Y_LOW: -1 });
+  const trainedWeights = randomPoints.map( point => ({
+    point,
+    actualTeam: getTeamForPoint(point)
+  })).reduce((weights, { point, actualTeam }) => {
+    const newWeights = train({ weights, point, actualTeam })
+    console.log(newWeights);
+    return newWeights;
+  }, startingWeights);
   return trainedWeights;
 }
 
-console.log(trainedWeights());
+const test = () => {
+  const sampleSize = 200;
+  const trainedWeights = trainWeights();
+  const randomPoints = generatePoints({ count: sampleSize });
+
+  const results = randomPoints.map((point) => ({
+    actualTeam: getTeamForPoint(point),
+    guess: guessTeamForPoint({ weights: trainedWeights, point })
+  })).filter(({ actualTeam, guess}) => actualTeam === guess);
+
+  console.log(`${results.length/sampleSize*100}%`);
+}
+test();
